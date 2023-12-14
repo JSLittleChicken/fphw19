4e4bfe39f71b3fd6c5d76aa1cac6f3c3b166483c
JSLittleChicken <oksana.petrovich.2019@gmail.com>
2023-12-15 00:54:44 +0200

    error fixed better

--- hw19.js
+++ hw19.js
@@ -1,9 +1,23 @@
-function makeFunc (number1) {
-    console.log(`sum(${number1}) = ${number1}`);
-    return function (number2) {
-        const result = number1 + number2
-        console.log(`sum(${number2}) = ${result}`);
-    }
+// function makeFunc(number1) {
+//     console.log(`sum(${number1}) = ${number1}`);
+//     return function (number2) {
+//         const result = number1 + number2
+//         console.log(`sum(${number2}) = ${result}`);
+//         return function (number3) {
+//             const resultTwo = result + number3
+//             console.log(`sum(${number3}) = ${resultTwo}`)
+//         }
+//     }
+// }
+
+
+let addNumber = 0;
+function sum(number) {
+    addNumber += number
+    return addNumber
 }
-let sum = makeFunc(3);
-sum(5);
+
+console.log(sum(3));
+console.log(sum(8));
+console.log(sum(20));
+
